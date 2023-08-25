c6ac8eae65214ac6247e856a3d5d4bf3ae459bf1
Ni27sh <eng.nishantgawad@gmail.com>
2023-08-25 10:08:34 +0530

    changed alternate components query

--- src/main/java/com/vca/controllers/AuthController.java
+++ src/main/java/com/vca/controllers/AuthController.java
@@ -1,92 +1,4 @@
-
-package com.vca.controllers;
-
-import java.util.HashMap;
-import java.util.Map;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.validation.BindingResult;
-import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-
-import com.vca.entity.Registration;
-import com.vca.repositories.RegistrationRepository;
-import com.vca.services.RegistrationService;
-
-import jakarta.validation.Valid;
-import response.ResponseHandler;
-
-@Validated
-@RestController
-@CrossOrigin(origins = "*")
-@RequestMapping("/api/auth")
-public class AuthController {
-	
-	@Autowired
-    private RegistrationService registrationService;
-
-	@PostMapping("/login")
-	public ResponseEntity<Object> Login( @RequestBody  Registration company, BindingResult bindingResult){
-		
-		try {
-	        // Authenticate the user here and generate a JWT token upon successful login
-	        // You should replace this with your actual authentication logic
-
-	        // If authentication is successful, generate a JWT token
-			// String jwtToken = generateJwtToken(loginRequest.getUsername()); // Replace with your token generation logic
-			Registration existComp=registrationService.findByUsername(company.getEmail());
-	    	if(existComp==null||!existComp.getPassword().equals(company.getPassword())) {
-	    		return ResponseEntity.badRequest().body("Invalid Credential");
-	    	}
-	    		Map<String, Object> responseData = new HashMap<>();
-	            responseData.put("status", "success");
-	          //  responseData.put("token", jwtToken);
-	            return ResponseEntity.status(HttpStatus.OK).body(responseData);
-	       
-		}catch(Exception e) {
-			// Handle authentication failure
-	        Map<String, Object> errorResponse = new HashMap<>();
-	        errorResponse.put("status", "error");
-	        errorResponse.put("message", "Authentication failed"); // Customize this message
-			return ResponseHandler.apiResponse(e.getMessage(), HttpStatus.UNAUTHORIZED, null);
-		}
-		
-	}
-	
-	@PostMapping("/register")
-	public ResponseEntity<Object> Register( @RequestBody @Valid Registration registretion,BindingResult bindingResult){
-		try {
-			
-			// Implement your registration logic here
-			if(registrationService.findByUsername(registretion.getEmail())!=null)
-	    	{
-	    		return ResponseEntity.badRequest().body("username already exist");
-	    	}
-			if (bindingResult.hasErrors()) {
-	            return ResponseEntity.badRequest().body("Validation errors");
-	        }
-
-		       registrationService.createRegistration(registretion);
-			
-			return ResponseHandler.apiResponse("Registration successful", HttpStatus.OK, null);
-		}catch(Exception e) {
-	        Map<String, Object> errorResponse = new HashMap<>();
-	        errorResponse.put("status", "error");
-	        errorResponse.put("message", "Authentication failed");
-			return ResponseHandler.apiResponse(e.getMessage(), HttpStatus.BAD_REQUEST, null);
-		}
-		
-	}
-
-}
+//
 //package com.vca.controllers;
 //
 //import java.util.HashMap;
@@ -95,6 +7,8 @@ public class AuthController {
 //import org.springframework.beans.factory.annotation.Autowired;
 //import org.springframework.http.HttpStatus;
 //import org.springframework.http.ResponseEntity;
+//import org.springframework.validation.BindingResult;
+//import org.springframework.validation.annotation.Validated;
 //import org.springframework.web.bind.annotation.CrossOrigin;
 //import org.springframework.web.bind.annotation.PostMapping;
 //import org.springframework.web.bind.annotation.RequestBody;
@@ -104,8 +18,10 @@ public class AuthController {
 //import com.vca.entity.Registration;
 //import com.vca.services.RegistrationService;
 //
+//import jakarta.validation.Valid;
 //import response.ResponseHandler;
 //
+//@Validated
 //@RestController
 //@CrossOrigin(origins = "*")
 //@RequestMapping("/api/auth")
@@ -115,7 +31,7 @@ public class AuthController {
 //    private RegistrationService registrationService;
 //
 //	@PostMapping("/login")
-//	public ResponseEntity<Object> Login(@RequestBody Registration company){
+//	public ResponseEntity<Object> Login( @RequestBody  Registration company, BindingResult bindingResult){
 //		
 //		try {
 //	        // Authenticate the user here and generate a JWT token upon successful login
@@ -143,20 +59,23 @@ public class AuthController {
 //	}
 //	
 //	@PostMapping("/register")
-//	public ResponseEntity<Object> Register(@RequestBody  Registration registretion){
+//	public ResponseEntity<Object> Register( @RequestBody @Valid Registration registretion, BindingResult bindingResult){
 //		try {
 //			
 //			// Implement your registration logic here
 //			if(registrationService.findByUsername(registretion.getEmail())!=null)
 //	    	{
-//	    		return ResponseEntity.badRequest().body("username already exist");
+//				return ResponseHandler.apiResponse("username already exist", HttpStatus.BAD_REQUEST, null);
 //	    	}
+//			if (bindingResult.hasErrors()) {
+//				return ResponseHandler.apiResponse("Validation response", HttpStatus.OK, null);
+//	        }
+//
 //		       registrationService.createRegistration(registretion);
 //			
 //			return ResponseHandler.apiResponse("Registration successful", HttpStatus.OK, null);
 //		}catch(Exception e) {
 //	        Map<String, Object> errorResponse = new HashMap<>();
-//	        errorResponse.put("status", "error");
 //	        errorResponse.put("message", "Authentication failed");
 //			return ResponseHandler.apiResponse(e.getMessage(), HttpStatus.BAD_REQUEST, null);
 //		}
@@ -164,4 +83,3 @@ public class AuthController {
 //	}
 //
 //}
-
